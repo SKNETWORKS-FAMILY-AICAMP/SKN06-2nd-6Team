8ecf1a3f417746e3e4211a91c30a3800745cd152
Yujin Chung <chungyuj33@gmail.com>
2024-11-14 10:59:25 +0900

    streamlit data 페이지 수정중

--- views/data.py
+++ views/data.py
@@ -5,31 +5,51 @@ import seaborn as sns
 
 
 def show_data():
-    st.header("\:bookmark_tabs: Exploratory Data Analysis")
+    st.header(":bookmark_tabs: Exploratory Data Analysis")
     data = pd.read_csv("data/waze_dataset.csv", index_col=0)
     # 데이터셋 기본 정보
-    st.subheader("\:one: Data Information", divider=True)
+    st.subheader(":one: Data Information", divider=True)
     st.markdown(
         """This dataset is supplied as part of the Google Advanced Data Analytics Professional Certificate program courses on Coursera.    
         According to Google, this dataset contains synthetic data created in partnership with Waze."""
     )
     st.divider()
     # describe()
-    st.subheader("\:two: Data Statistics", divider=True)
+    st.subheader(":two: Data Statistics", divider=True)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown(
+            f"""
+            | Dataset Statistics | Value |
+| --- | --- |
+| 변수의 개수 | {data.shape[1]} |
+| 데이터의 개수 | {data.shape[0]} |
+| 결측치의 개수 | {data.isnull().sum().sum()} |
+| 중복된 행의 개수 | {data.duplicated().sum()} |
+            """
+        )
+    with col2:
+        st.markdown(
+            f"""
+                    | Variable Types | Value |
+| --- | --- |
+| Numerical | {data.select_dtypes(include=['int64', 'float64']).shape[1]} |
+| Categorical | {data.select_dtypes(include=['object']).shape[1]} |"""
+        )
     st.dataframe(data.describe())
     st.divider()
     # head()
-    st.subheader("\:three: Data Head", divider=True)
+    st.subheader(":three: Data Head", divider=True)
     st.dataframe(data.head())
     st.divider()
     # 시각화
-    st.subheader("\:four: Data Visualization", divider=True)
-    col1, col2 = st.columns([0.3, 0.7])
-    with col1:
+    st.subheader(":four: Data Visualization", divider=True)
+    col3, col4 = st.columns([0.3, 0.7])
+    with col3:
         columns = data.columns
         columns = columns.drop("label")
         selected_column = st.selectbox("Select Column", columns)
-    with col2:
+    with col4:
         fig, ax = plt.subplots()
         if selected_column != "device":
             sns.kdeplot(
@@ -64,4 +84,22 @@ def show_data():
         sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
         ax.set_title(f"{selected_column} 이탈 분포", fontsize=16)
         st.pyplot(fig)
+    # 상관계수
+    st.subheader(":five: Correlation", divider=True)
+    co15, col6, col7 = st.columns([0.1, 0.8, 0.1])
+    with col6:
+        fig, ax = plt.subplots()
+        fig.set_figwidth(8)
+        fig.set_figheight(8)
+        sns.heatmap(
+            data=data.select_dtypes(include="number").corr(),
+            annot=True,
+            fmt=".2f",
+            linewidths=0.5,
+            cmap="Blues",
+            ax=ax,
+        )
+        ax.set_title("상관계수", fontsize=16)
+        st.pyplot(fig)
+    st.divider()
     # 추가 시각화? 정보?
