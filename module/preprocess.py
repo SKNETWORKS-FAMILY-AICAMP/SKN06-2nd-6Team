1159404ec5e3bf1341881dcf70190f97dfb7f961
Yujin Chung <chungyuj33@gmail.com>
2024-11-19 09:30:53 +0900

    전처리기 저장

--- module/preprocess.py
+++ module/preprocess.py
@@ -1,18 +1,17 @@
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import pandas as pd
 
-
 class Preprocessor:
-    def __init__(self, data):
-        self.data = data
-
+    def __init__(self, data_path):
+        self.data_path = data_path
     def preprocess(self):
+        data = pd.read_csv(self.data_path, index_col=0)
         # 결측치 삭제
-        self.data.dropna(inplace=True)
+        data.dropna(inplace=True)
         # label encoding
         l_encoder = LabelEncoder()
-        if "label" in self.data.columns:
-            self.data["label"] = l_encoder.fit_transform(self.data["label"])
+        if "label" in data.columns:
+            data["label"] = l_encoder.fit_transform(data["label"])
         # one hot encoding
-        self.data = pd.get_dummies(self.data, columns=["device"])
-        return self.data
+        data = pd.get_dummies(data, columns=["device"])
+        return data
