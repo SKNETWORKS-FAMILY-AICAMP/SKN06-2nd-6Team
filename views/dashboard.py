b163d631beb72f28b969e1b50f757a570210433f
Yujin Chung <chungyuj33@gmail.com>
2024-11-21 15:18:22 +0900

    dashboard ROC 이미지 삽입

--- views/dashboard.py
+++ views/dashboard.py
@@ -30,11 +30,10 @@ def graphs(metrics_dict, key, mode="metric", graph="Bar", lr="ml"):
                 data=nivo_data,
                 keys=["pred_test"],
                 indexBy="metric",
-                margin={"top": 70, "right": 40, "bottom": 40, "left": 40},
+                margin={"top": 20, "right": 40, "bottom": 40, "left": 40},
                 colors={"scheme": "paired"},
                 colorBy="indexValue",
-                labelPosition="end",
-                labelOffset={8},
+                labelPosition="start",
                 theme={
                     "background": "#FFFFFF",
                     "textColor": "#31333F",
@@ -114,31 +113,44 @@ def member_image(handle_layout_change):
     # 멤버별 이미지
     layout = [
         # Parameters: element_identifier, x_pos, y_pos, width, height
+        dashboard.Item("team", 0, 0, 8, 0.2, isResizable=False),
+        dashboard.Item("period", 8, 0, 2, 0.2, isResizable=False),
         dashboard.Item("member_image_1", 0, 2, 2.5, 1.5, isResizable=False),
         dashboard.Item("member_image_2", 2.5, 2, 2.5, 1.5, isResizable=False),
         dashboard.Item("member_image_3", 5, 2, 2.5, 1.5, isResizable=False),
         dashboard.Item("member_image_4", 7.5, 2, 2.5, 1.5, isResizable=False),
     ]
     with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+        with mui.Box(key="team"):
+            mui.Typography(
+                "6Team 아무래도 귀엽조", variant="h6", sx={"color": "#000000"}
+            )
+        with mui.Box(key="period"):
+            mui.Typography(
+                "2024.11.13 ~ 2024.11.14", variant="h7", sx={"color": "#000000"}
+            )
+
         # 1: 승현님, 2: 서윤님, 3: 하은님 4: 코딩 에러 으아아
         image_1 = "https://raw.githubusercontent.com/SKNETWORKS-FAMILY-AICAMP/SKN06-2nd-6Team/refs/heads/main/image/%EC%8A%B9%ED%98%84%EB%8B%98%EB%84%A4%EA%B7%80%EC%9A%94%EB%AF%B8.png"
         image_2 = "https://raw.githubusercontent.com/SKNETWORKS-FAMILY-AICAMP/SKN06-2nd-6Team/refs/heads/main/image/%EC%84%9C%EC%9C%A4%EB%8B%98%EB%84%A4%EA%B7%80%EC%9A%94%EB%AF%B8.png"
         image_3 = "https://raw.githubusercontent.com/SKNETWORKS-FAMILY-AICAMP/SKN06-2nd-6Team/refs/heads/main/image/%ED%95%98%EC%9D%80%EB%8B%98%EB%84%A4%EA%B7%80%EC%9A%94%EB%AF%B8.png"
         image_4 = "https://raw.githubusercontent.com/SKNETWORKS-FAMILY-AICAMP/SKN06-2nd-6Team/refs/heads/main/image/%EB%B6%80%EC%8B%A4%EA%B0%90%EC%9E%90.jpeg"
-        with mui.Paper(key="member_image_1"):
+        with mui.Paper(key="member_image_2"):
             mui.Box(
                 component="img",
                 src=image_1,
                 alt="김승현",
                 sx={"width": "100%", "height": "100%"},
             ),
-        with mui.Paper(key="member_image_2"):
+            mui.Box("김승현/DL")
+        with mui.Paper(key="member_image_4"):
             mui.Box(
                 component="img",
                 src=image_2,
                 alt="박서윤",
                 sx={"width": "100%", "height": "100%"},
             ),
+            mui.Box("박서윤/ML")
         with mui.Paper(key="member_image_3"):
             mui.Box(
                 component="img",
@@ -146,13 +158,15 @@ def member_image(handle_layout_change):
                 alt="백하은",
                 sx={"width": "100%", "height": "100%"},
             ),
-        with mui.Paper(key="member_image_4"):
+            mui.Box("백하은/결과보고서, ML, streamlit")
+        with mui.Paper(key="member_image_1"):
             mui.Box(
                 component="img",
                 src=image_4,
                 alt="정유진",
                 sx={"width": "100%", "height": "100%"},
             ),
+            mui.Box("정유진/전처리, DL, streamlit")
 
 
 def show_dashboard():
@@ -176,9 +190,9 @@ def show_dashboard():
     with elements("dashboard"):
         layout = [
             # Parameters: element_identifier, x_pos, y_pos, width, height
-            dashboard.Item("prediction_image_1", 0, 2, 3.2, 2),
-            dashboard.Item("prediction_image_2", 3.5, 2, 3.2, 2),
-            dashboard.Item("prediction_image_3", 7, 2, 3.2, 2),
+            dashboard.Item("prediction_image_1", 7, 0, 3, 1.5),
+            dashboard.Item("prediction_image_2", 7, 4, 3, 1.5),
+            dashboard.Item("prediction_image_3", 0, 0, 7, 3),
         ]
 
         def handle_layout_change(updated_layout):
@@ -199,7 +213,7 @@ def show_dashboard():
                 lr="ml",
             )
             graph_matrix(y_test, ml_y_pred_list, "prediction_image_2")
-            image = "image/ML/ROC.png"
+            image = "https://raw.githubusercontent.com/SKNETWORKS-FAMILY-AICAMP/SKN06-2nd-6Team/refs/heads/main/image/ML/ROC.png"
             mui.Box(
                 key="prediction_image_3",
                 component="img",
@@ -230,7 +244,7 @@ def show_dashboard():
                 mode="roc",
                 lr="dl",
             )
-            image = "image/ML/ROC.png"
+            image = "https://raw.githubusercontent.com/SKNETWORKS-FAMILY-AICAMP/SKN06-2nd-6Team/refs/heads/main/image/DL/ROC.png"
             mui.Box(
                 key="prediction_image_3",
                 component="img",
